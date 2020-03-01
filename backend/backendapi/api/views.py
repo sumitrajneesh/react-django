62410bd2bb186312396b86687a4cefc5c05a732c
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-03-01 22:46:35 +0530

    Commit

--- backend/backendapi/api/views.py
+++ backend/backendapi/api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer,BookSerializer
+from .models import Book
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset  = User.objects.all()
     serializer_class = UserSerializer
+
+   
+class BookViewSet(viewsets.ModelViewSet):
+    queryset  = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication,]
+    permissions_classes = [IsAuthenticated,]
