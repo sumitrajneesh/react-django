ff68d56314c1d5784d83199f0ab63086e6d13246
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-03-01 17:32:56 +0530

    Commit

--- backend/backendapi/backendapi/settings.py
+++ backend/backendapi/backendapi/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +73,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backendapi.wsgi.application'
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
 
 
 # Database
